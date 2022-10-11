6547741ab6e6d180c3597932e463ea3ffc608d15
SuperYuro <a.gpgtdmgp@gmail.com>
2022-10-11 19:50:32 +0900

    Change Neovim config to install language server automatically

--- nvim/after/plugin/mason.rc.lua
+++ nvim/after/plugin/mason.rc.lua
@@ -4,33 +4,17 @@ local status2, lspconfig = pcall(require, 'mason-lspconfig')
 if (not status2) then return end
 
 mason.setup {
-  ui = {
-    icons = {
-      package_installed = "✓",
-      package_pending = "➜",
-      package_uninstalled = "✗"
+    ui = {
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+        }
     }
-  }
 }
 lspconfig.setup {
-  ensure_installed = {
-    'arduino_language_server',
-    'bashls',
-    'clangd',
-    'cmake',
-    'cssls',
-    'cssmodules_ls',
-    'dockerls',
-    'html',
-    'jsonls',
-    'tsserver',
-    'texlab',
-    'sumneko_lua',
-    'marksman',
-    'pylsp',
-    'rust_analyzer',
-    'taplo',
-    'tailwindcss',
-    'vimls',
-  }
+    ensure_installed = {},
+    automatic_installation = true
 }
+
+vim.keymap.set('n', 'gis', ':LspInstall<Return>', { silent = true })
