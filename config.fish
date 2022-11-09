c2f6bb5e7e42a77de603d2d196225d270adad0a3
SuperYuro <a.gpgtdmgp@gmail.com>
2022-11-09 13:20:16 +0900

    Change colorscheme, add new alias

--- config.fish
+++ config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -g theme_color_scheme light
+set -g theme_color_scheme nord
 set -g theme_nerd_fonts yes
 
 set -g theme_display_git_default_branch yes
@@ -10,7 +10,6 @@ set -g theme_git_default_branches yes
 
 set -g theme_title_display_process yes
 set -g theme_title_display_path yes
-set -g theme_title_display_user yes
 set -g theme_title_use_abbreviated_path yes
 
 # set -g theme_display_ruby yes
@@ -25,10 +24,14 @@ alias l 'ls -1'
 alias la 'ls -lA'
 alias ll 'ls -lh'
 
+alias tm tmux
 alias c cd
 alias g git
 alias v vim
 alias md mkdir
+alias dp 'docker compose'
 alias bi 'acpi --battery --details'
+
+set fish_greeting
 
 node -v
