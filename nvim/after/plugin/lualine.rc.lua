ba044453d75d7a4718a6cd0c113d5390cad55cb0
SuperYuro <a.gpgtdmgp@gmail.com>
2022-11-13 17:02:25 +0900

    Move diagnostics location

--- nvim/after/plugin/lualine.rc.lua
+++ nvim/after/plugin/lualine.rc.lua
@@ -25,35 +25,34 @@ lualine.setup {
     sections = {
         lualine_a = { 'mode' },
         lualine_b = { 'branch' },
-        lualine_c = {
-            {
-                'filename',
-                file_status = true,
-                newfile_status = false,
-                path = 1,
+        lualine_c = { {
+            'filename',
+            file_status = true,
+            newfile_status = false,
+            path = 1
+        } },
+        lualine_x = { {
+            'diagnostics',
+            sources = { 'nvim_diagnostic', 'nvim_lsp' },
+            sections = { 'error', 'warn', 'info', 'hint' },
+            diagnostics_color = {
+                error = 'DiagnosticError',
+                warn = 'DiagnosticsWarn',
+                info = 'DiagnosticsInfo',
+                hint = 'DiagnosticsHint',
             },
+            symbols = {
+                error = 'E',
+                warn = 'W',
+                info = 'I',
+                hint = 'H',
+            }
         },
-        lualine_x = { 'fileformat', 'filetype' },
+            'fileformat',
+            'filetype'
+        },
         lualine_y = { 'progress', 'location' },
-        lualine_z = {
-            {
-                'diagnostics',
-                sources = { 'nvim_diagnostic', 'nvim_lsp' },
-                sections = { 'error', 'warn', 'info', 'hint' },
-                diagnostics_color = {
-                    error = 'DiagnosticError',
-                    warn = 'DiagnosticsWarn',
-                    info = 'DiagnosticsInfo',
-                    hint = 'DiagnosticsHint',
-                },
-                symbols = {
-                    error = 'E',
-                    warn = 'W',
-                    info = 'I',
-                    hint = 'H',
-                },
-            },
-        },
+        lualine_z = {}
     },
     inactive_sections = {
         lualine_a = {},
@@ -64,34 +63,30 @@ lualine.setup {
         lualine_z = {}
     },
     tabline = {
-        lualine_a = {
-            {
-                'tabs',
-                mode = 1,
-                tabs_color = {
-                    active = 'lualine_a_normal',
-                    inactive = 'lualine_a_inactive',
-                },
+        lualine_a = { {
+            'tabs',
+            mode = 1,
+            tabs_color = {
+                active = 'lualine_a_normal',
+                inactive = 'lualine_a_inactive',
             },
-        },
-        lualine_b = {
-            {
-                'buffers',
-                show_filename_only = true,
-                show_modified_status = true,
-                mode = 2,
-                buffers_color = {
-                    -- Same values as the general color option can be used here.
-                    active = 'lualine_b_normal', -- Color for active buffer.
-                    inactive = 'lualine_b_inactive', -- Color for inactive buffer.
-                },
-                symbols = {
-                    modified = ' ●', -- Text to show when the buffer is modified
-                    alternate_file = '#', -- Text to show to identify the alternate file
-                    directory = 'D', -- Text to show when the buffer is a directory
-                },
+        } },
+        lualine_b = { {
+            'buffers',
+            show_filename_only = true,
+            show_modified_status = true,
+            mode = 2,
+            buffers_color = {
+                -- Same values as the general color option can be used here.
+                active = 'lualine_b_normal', -- Color for active buffer.
+                inactive = 'lualine_b_inactive', -- Color for inactive buffer.
             },
-        }
+            symbols = {
+                modified = 'M', -- Text to show when the buffer is modified
+                alternate_file = '#', -- Text to show to identify the alternate file
+                directory = 'D', -- Text to show when the buffer is a directory
+            }
+        } }
     },
     winbar = {},
     inactive_winbar = {},
