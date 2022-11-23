1e447303cd965258cc823fddf5504bcccfb3fbe5
SuperYuro <a.gpgtdmgp@gmail.com>
2022-11-23 20:41:13 +0900

    Update prezto install script

--- install_zprezto.zsh
+++ install_zprezto.zsh
@@ -1,7 +1,15 @@
-ln -s ~/dotfiles/zprezto ~/.zprezto
-
 setopt EXTENDED_GLOB
 
+# Cline repository
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+# Delete existing rc directory
+rm -rf ${ZDOTDIR:-$HOME}/.zprezto/runcoms
+
+# Link rc directory
+ln -s ${ZDOTDIR:-$HOME}/dotfiles/runcoms ${ZDOTDIR:-$HOME}/.zprezto/runcoms
+
+# Link rc files
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
     ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
