836005753da32882c34db60233baec007f23ec61
SuperYuro <a.gpgtdmgp@gmail.com>
2022-12-09 00:37:39 +0900

    Add zsh plugins install script

---
+++ zsh_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-history-substring-search ~/.zsh/zsh-history-substring-search
+git clone https://github.com/romkatv/powerlevel10k.git ~/.zsh/powerlevel10k
