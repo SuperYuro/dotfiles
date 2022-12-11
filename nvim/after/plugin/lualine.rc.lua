09956434e4e5099bbe1c3d8377fda721980008c2
SuperYuro <a.gpgtdmgp@gmail.com>
2022-12-11 16:21:00 +0900

    Update neovim config

--- nvim/after/plugin/lualine.rc.lua
+++ nvim/after/plugin/lualine.rc.lua
@@ -5,7 +5,7 @@ end
 
 lualine.setup({
 	options = {
-		icons_enabled = false,
+		icons_enabled = true,
 		theme = "auto",
 		-- component_separators = { left = "|", right = "|" },
 		-- section_separators = { left = "", right = "" },
