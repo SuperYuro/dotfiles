4e165bfed4eeb4c5e9144ae137032aa7c9ff445b
SuperYuro <a.gpgtdmgp@gmail.com>
2022-12-21 00:08:18 +0900

    Add rbenv and pyenv installation

--- zsh_install.sh
+++ zsh_install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# Install rbenv
+git clone https://github.com/rbenv/rbenv ~/.rbenv
+git clone https://github.com/rbenv/ruby-build ~/.ruby-build
+
+# Install pyenv
+git clone https://github.com/pyenv/pyenv ~/.pyenv
+
+# Install zsh extensions
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-history-substring-search ~/.zsh/zsh-history-substring-search
-git clone https://github.com/romkatv/powerlevel10k.git ~/.zsh/powerlevel10k
