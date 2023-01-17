555e333df8d7dfda0c7be85fe4035510b1a34e91
SuperYuro <a.gpgtdmgp@gmail.com>
2023-01-17 18:02:34 +0900

    Fix lspsaga config

--- config/nvim/after/plugin/lspsaga.rc.lua
+++ config/nvim/after/plugin/lspsaga.rc.lua
@@ -1,10 +1,10 @@
 local status, saga = pcall(require, "lspsaga")
 if not status then
-	return
+    return
 end
 
-saga.init_lsp_saga({
-	server_filetype_map = {},
+saga.setup({
+    server_filetype_map = {},
 })
 
 local opts = { noremap = true, silent = true }
