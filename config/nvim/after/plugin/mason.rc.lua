7a159be2e054c747d397c11e00c90e317360c709
SuperYuro <a.gpgtdmgp@gmail.com>
2023-01-27 09:35:09 +0900

    Format code

--- config/nvim/after/plugin/mason.rc.lua
+++ config/nvim/after/plugin/mason.rc.lua
@@ -1,30 +1,35 @@
 local status, mason = pcall(require, "mason")
 if not status then
-    return
+	return
 end
 local status2, lspconfig = pcall(require, "mason-lspconfig")
 if not status2 then
-    return
+	return
 end
 
 mason.setup({
-    ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗",
-            -- package_installed = "I",
-            -- package_pending = ">",
-            -- package_uninstalled = "x",
-        },
-    },
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+			-- package_installed = "I",
+			-- package_pending = ">",
+			-- package_uninstalled = "x",
+		},
+	},
 })
 lspconfig.setup({
-    ensure_installed = {
-        "pyright",
-        "sumneko_lua",
-    },
-    automatic_installation = true,
+	ensure_installed = {
+		"bashls",
+		"jsonls",
+		"taplo",
+		"yamlls",
+		"dockerls",
+		"rust_analyzer",
+		"sumneko_lua",
+	},
+	automatic_installation = true,
 })
 
 vim.keymap.set("n", "gis", ":LspInstall<Return>", { silent = true })
