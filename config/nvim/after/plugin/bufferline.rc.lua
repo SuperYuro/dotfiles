a865672c7d17bfa43e3502da1c0fb791d57139e0
SuperYuro <a.gpgtdmgp@gmail.com>
2023-01-27 11:16:08 +0900

    Change colorscheme * Before: Nord * Current: Solarized Dark

--- config/nvim/after/plugin/bufferline.rc.lua
+++ config/nvim/after/plugin/bufferline.rc.lua
@@ -1,44 +1,44 @@
 local status, bufferline = pcall(require, "bufferline")
 if not status then
-    return
+	return
 end
 
 bufferline.setup({
-    options = {
-        mode = "buffers",
-        separator_style = "slant",
-        always_show_bufferline = true,
-        show_buffer_close_icons = false,
-        show_close_icon = false,
-        color_icons = true,
-        modified_icon = "●",
-        close_icon = "",
-        left_trunc_marker = "",
-        right_trunc_marker = "",
-    },
-    highlights = {
-        separator = {
-            fg = "#3b4252", -- normal black
-            bg = "#2e3440", -- background
-        },
-        separator_selected = {
-            fg = "#3b4252", -- normal black
-        },
-        background = {
-            fg = "#4c566a", -- light theme foreground or bright black
-            bg = "#2e3440", -- background
-        },
-        buffer_selected = {
-            fg = "#eceff4", -- light theme background or bright white
-        },
-        fill = {
-            bg = "#3b4252", -- normal black
-            underline = true,
-            undercurl = true,
-            italic = true,
-            bold = true,
-        },
-    },
+	options = {
+		mode = "buffers",
+		separator_style = "slant",
+		always_show_bufferline = true,
+		show_buffer_close_icons = false,
+		show_close_icon = false,
+		color_icons = true,
+		modified_icon = "●",
+		close_icon = "",
+		left_trunc_marker = "",
+		right_trunc_marker = "",
+	},
+	highlights = {
+		separator = {
+			fg = "#073642", -- normal black
+			bg = "#002b36", -- background
+		},
+		separator_selected = {
+			fg = "#073642", -- normal black
+		},
+		background = {
+			fg = "#657b83", -- light theme foreground or bright black
+			bg = "#002b36", -- background
+		},
+		buffer_selected = {
+			fg = "#fdf6e3", -- light theme background or bright white
+		},
+		fill = {
+			bg = "#073642", -- normal black
+			underline = true,
+			undercurl = true,
+			italic = true,
+			bold = true,
+		},
+	},
 })
 
 vim.api.nvim_set_keymap("n", "<Tab>", "<cmd>BufferLineCycleNext<cr>", {})
