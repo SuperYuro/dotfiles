34c1bbe0c1ff6a0fb065325999360971f3500a39
SuperYuro <a.gpgtdmgp@gmail.com>
2023-03-08 19:14:58 +0900

    install desktop/laptop: fix github-cli name

--- installs/desktop/install.sh
+++ installs/desktop/install.sh
@@ -8,7 +8,7 @@ makepkg -si --noconfirm
 cd ~/
 
 # Misc
-sudo pacman -S xterm xsel curl ripgrep gh the_silver_searcher --noconfirm
+sudo pacman -S xterm xsel curl ripgrep github-cli the_silver_searcher --noconfirm
 
 # Shell
 sudo pacman -S zsh peco --noconfirm
