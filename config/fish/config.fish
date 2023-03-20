1ad4c3c9a095ce3fa136c87f23e26c720e9e8227
SuperYuro <a.gpgtdmgp@gmail.com>
2023-03-20 18:57:28 +0900

    config: add fish cofig

---
+++ config/fish/config.fish
@@ -0,0 +1,40 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -g theme_color_scheme nord
+
+set -g theme_display_git yes
+set -g theme_display_git_default_branch yes
+
+set -g theme_display_virtualenv yes
+set -g theme_display_ruby yes
+set -g theme_display_nord yes
+
+set -g theme_display_user ssh
+set -g theme_display_hostname ssh
+
+set -g theme_show_exit_status yes
+
+alias l 'ls -1A'
+alias ll 'ls -lh'
+alias la 'ls -lA'
+
+alias c cd
+alias th touch
+alias md mkdir
+
+alias g git
+alias v nvim
+alias tm tmux
+alias dp 'docker compose'
+
+if type xsel >/dev/null 2>&1
+    alias copy 'xsel --input --clipboard'
+end
+
+alias cv 'cd ~/Development'
+
+alias rl 'exec fish'
+
+pyenv init - | source
