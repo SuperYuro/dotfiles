4592bec0861101098593dfa39807c14469c79d5e
SuperYuro <a.gpgtdmgp@gmail.com>
2023-03-26 19:14:50 +0900

    nvim: change colorscheme nord -> zellner

--- config/nvim/lua/highlights.lua
+++ config/nvim/lua/highlights.lua
@@ -3,4 +3,5 @@ vim.opt.termguicolors = true
 vim.opt.winblend = 0
 vim.opt.wildoptions = "pum"
 vim.opt.pumblend = 5
-vim.opt.background = "dark"
+vim.opt.background = "light"
+vim.cmd([[colorscheme zellner]])
