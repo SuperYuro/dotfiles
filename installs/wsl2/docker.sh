50224e81307cc819aaf044024f08504fcbf6622c
SuperYuro <a.gpgtdmgp@gmail.com>
2023-04-09 19:27:24 +0900

    install wsl: fix docker install script

--- installs/wsl2/docker.sh
+++ installs/wsl2/docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get remove docker docker.io containerd runc
 
 sudo apt-get update
 sudo apt-get install ca-certificates curl gnupg lsb-release
