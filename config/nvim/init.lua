a3ba08f2d03ccb43a7bd4db464b98ba58af95da4
SuperYuro <a.gpgtdmgp@gmail.com>
2023-04-23 17:37:33 +0900

    nvim: move config files to SuperYuro/

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,18 +1,18 @@
-require("base")
-require("highlights")
-require("maps")
-require("plugins")
+require("SuperYuro.base")
+require("SuperYuro.highlights")
+require("SuperYuro.maps")
+require("SuperYuro.plugins")
 
 local has = function(x)
 	return vim.fn.has(x) == 1
 end
 
-local is_mac = has("macunix")
-local is_win = has("win32")
+local is_mac = has("SuperYuro.macunix")
+local is_win = has("SuperYuro.win32")
 
 if is_mac then
-	require("macos")
+	require("SuperYuro.macos")
 end
 if is_win then
-	require("windows")
+	require("SuperYuro.windows")
 end
