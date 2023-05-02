03a41fb34af8ac0db3285136ddf438e45f429156
SuperYuro <a.gpgtdmgp@gmail.com>
2023-05-02 23:59:29 +0900

    nvim: add some plugins

--- config/nvim/lua/SuperYuro/plugins.lua
+++ config/nvim/lua/SuperYuro/plugins.lua
@@ -1,297 +1,286 @@
 local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-    if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-        vim.cmd([[packadd packer.nvim]])
-        return true
-    end
-    return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 
 local packer_bootstrap = ensure_packer()
 
 return require("packer").startup(function(use)
-    use("wbthomason/packer.nvim")
+	use("wbthomason/packer.nvim")
 
-    -- Colorscheme
-    use({
-        "shaunsingh/nord.nvim",
-        config = function()
-            vim.cmd([[colorscheme nord]])
-        end,
-    })
+	-- Colorscheme
+	use({
+		"shaunsingh/nord.nvim",
+		config = function()
+			vim.cmd([[colorscheme nord]])
+		end,
+	})
 
-    -- Fuzzy finder
-    use({
-        "nvim-telescope/telescope.nvim",
-        requires = {
-            "nvim-lua/plenary.nvim",
-            "nvim-telescope/telescope-file-browser.nvim", -- File browser
-            "chip/telescope-software-licenses.nvim", -- Search Software License
-            "xiyaowong/telescope-emoji.nvim",    -- Search emoji
-            "fcying/telescope-ctags-outline.nvim", -- Get outline
-            "LinArcX/telescope-env.nvim",        -- Show environment variables
-        },
-        config = function()
-            require("SuperYuro.config.telescope")
-        end,
-    })
+	-- Jump anywhere
+	use({
+		"phaazon/hop.nvim",
+		branch = "v2",
+		config = function()
+			require("hop").setup()
+			vim.keymap.set("n", "<space><space>", ":HopWord<CR>", { silent = true, noremap = true })
+		end,
+	})
 
-    -- Statusline
-    use({
-        "nvim-lualine/lualine.nvim",
-        config = function()
-            require("lualine").setup({
-                sections = {
-                    lualine_a = { "mode" },
-                    lualine_b = { "branch" },
-                    lualine_c = {
-                        {
-                            "filename",
-                            file_status = true,
-                            newfile_status = true,
-                            path = 1,
-                        },
-                    },
-                    lualine_x = {
-                        "fileformat",
-                        "filetype",
-                    },
-                    lualine_y = {
-                        "diagnostics",
-                        "diff",
-                    },
-                    lualine_z = {
-                        "progress",
-                        "location",
-                    },
-                },
-            })
-        end,
-    })
+	-- Notify daemon
+	use({
+		"rcarriga/nvim-notify",
+		config = function()
+			vim.notify = require("notify")
+		end,
+	})
 
-    -- Tabbar
-    use({
-        "akinsho/bufferline.nvim",
-        requires = { "nvim-tree/nvim-web-devicons" },
-        tag = "*",
-        config = function()
-            require("bufferline").setup({
-                options = {
-                    mode = "tabs",
-                    separator_style = "slant",
-                    always_show_bufferline = true,
-                },
-            })
-            vim.api.nvim_set_keymap("n", "<TAB>", "<cmd>BufferLineCycleNext<cr>", {})
-            vim.api.nvim_set_keymap("n", "<S-TAB>", "<cmd>BufferLineCyclePrev<cr>", {})
-        end,
-    })
+	-- Show next key
+	use({
+		"folke/which-key.nvim",
+		config = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+			require("which-key").setup({})
+		end,
+	})
 
-    -- Syntax Highlighting
-    use({
-        "nvim-treesitter/nvim-treesitter",
-        run = ":TSUpdate",
-        requires = {
-            "windwp/nvim-ts-autotag", -- Pair HTML tags automatically
-            "mrjones2014/nvim-ts-rainbow", -- Rainbow brackets
-        },
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = {
-                    "arduino", -- Arduino
-                    "bash", -- Bash
-                    "c", -- C
-                    "c_sharp", -- C#
-                    "comment", -- Comments like TODO: or FIXME:
-                    "cpp", -- C++
-                    "css", -- CSS
-                    "dockerfile", -- Dockerfile
-                    "fish", -- Fish
-                    "gitcommit", -- git commit
-                    "gitignore", -- .gitignore
-                    "haskell", -- Haskell
-                    "html", -- HTML
-                    "htmldjango", -- Django HTML
-                    "javascript", -- JavaScript
-                    "json", -- Json
-                    "lua", -- Lua
-                    "make", -- Makefile
-                    "markdown", -- Markdown, required by lspsaga
-                    "markdown_inline", -- Markdown, required by lspsaga
-                    "python", -- Python
-                    "rust", -- Rust
-                    "toml", -- TOML
-                    "tsx", -- TypeScriptReact
-                    "typescript", -- TypeScript
-                    "yaml", -- YAML
-                },
-                autotag = {
-                    enable = true,
-                },
-                rainbow = {
-                    enable = true,
-                    extended_mode = true,
-                    max_file_lines = nil,
-                },
-            })
-            require("nvim-ts-autotag").setup()
-        end,
-    })
+	-- Fuzzy finder
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-file-browser.nvim", -- File browser
+			"chip/telescope-software-licenses.nvim", -- Search Software License
+			"xiyaowong/telescope-emoji.nvim", -- Search emoji
+			"fcying/telescope-ctags-outline.nvim", -- Get outline
+			"LinArcX/telescope-env.nvim", -- Show environment variables
+		},
+		config = function()
+			require("SuperYuro.config.telescope")
+		end,
+	})
 
-    -- LSP
-    use({
-        "neovim/nvim-lspconfig",
-        requires = {
-            {
-                "williamboman/mason.nvim",
-                run = ":MasonUpdate",
-            },
-            {
-                "williamboman/mason-lspconfig.nvim",
-            },
-            {
-                "hrsh7th/cmp-nvim-lsp",
-            },
-        },
-        config = function()
-            require("SuperYuro.config.nvim-lspconfig")
-        end,
-    })
+	-- Statusline
+	use({
+		"nvim-lualine/lualine.nvim",
+		config = function()
+			require("lualine").setup({
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch" },
+					lualine_c = {
+						{
+							"filename",
+							file_status = true,
+							newfile_status = true,
+							path = 1,
+						},
+					},
+					lualine_x = { "fileformat", "filetype" },
+					lualine_y = { "diagnostics", "diff" },
+					lualine_z = { "progress", "location" },
+				},
+			})
+		end,
+	})
 
-    -- LSP UI
-    use({
-        "glepnir/lspsaga.nvim",
-        opt = true,
-        branch = "main",
-        event = "LspAttach",
-        config = function()
-            require("SuperYuro.config.lspsaga")
-        end,
-    })
+	-- Tabbar
+	use({
+		"akinsho/bufferline.nvim",
+		requires = { "nvim-tree/nvim-web-devicons" },
+		tag = "*",
+		config = function()
+			require("bufferline").setup({
+				options = {
+					mode = "tabs",
+					separator_style = "slant",
+					always_show_bufferline = true,
+				},
+			})
+			vim.api.nvim_set_keymap("n", "<TAB>", "<cmd>BufferLineCycleNext<cr>", {})
+			vim.api.nvim_set_keymap("n", "<S-TAB>", "<cmd>BufferLineCyclePrev<cr>", {})
+		end,
+	})
 
-    -- Diagnostics
-    use({
-        "folke/trouble.nvim",
-        config = function()
-            require("trouble").setup()
-        end,
-    })
+	-- Syntax Highlighting
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+		requires = {
+			"windwp/nvim-ts-autotag", -- Pair HTML tags automatically
+			"mrjones2014/nvim-ts-rainbow", -- Rainbow brackets
+			"RRethy/nvim-treesitter-endwise", -- Complete end automatically
+		},
+		config = function()
+			require("SuperYuro.config.treesitter")
+		end,
+	})
 
-    -- Format and Lint
-    use({
-        "jose-elias-alvarez/null-ls.nvim",
-        requires = {
-            "nvim-lua/plenary.nvim",
-        },
-        config = function()
-            require("SuperYuro.config.null-ls")
-        end,
-    })
+	-- LSP
+	use({
+		"neovim/nvim-lspconfig",
+		requires = {
+			{
+				"williamboman/mason.nvim",
+				run = ":MasonUpdate",
+			},
+			{ "williamboman/mason-lspconfig.nvim" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+		},
+		config = function()
+			require("SuperYuro.config.nvim-lspconfig")
+		end,
+	})
 
-    -- Completion
-    use({
-        "hrsh7th/nvim-cmp",
-        requires = {
-            "onsails/lspkind.nvim",        -- Icons
-            "hrsh7th/cmp-nvim-lsp",        -- from LSP
-            "hrsh7th/cmp-buffer",          -- from Buffer
-            "hrsh7th/cmp-path",            -- from file path
-            "hrsh7th/cmp-vsnip",           -- from snippet
-            "hrsh7th/vim-vsnip",           -- from snippet
-            "hrsh7th/cmp-nvim-lsp-document-symbol", -- from document symbol
-            "hrsh7th/cmp-nvim-lsp-signature-help", -- from signatures
-        },
-        config = function()
-            require("SuperYuro.config.completions")
-        end,
-    })
+	-- LSP UI
+	use({
+		"glepnir/lspsaga.nvim",
+		opt = true,
+		branch = "main",
+		event = "LspAttach",
+		config = function()
+			require("SuperYuro.config.lspsaga")
+		end,
+	})
 
-    -- Git
-    use({
-        "lewis6991/gitsigns.nvim",
-        confg = function()
-            require("gitsigns").setup()
-        end,
-    })
+	-- Diagnostics
+	use({
+		"folke/trouble.nvim",
+		config = function()
+			require("trouble").setup({})
+		end,
+	})
 
-    -- Color preview
-    use({
-        "NvChad/nvim-colorizer.lua",
-        config = function()
-            require("colorizer").setup({
-                RGB = true,
-                RRGGBB = true,
-                RRGGBBAA = false,
-                AARRGGBB = false,
-                names = false,
+	-- Format and Lint
+	use({
+		"jose-elias-alvarez/null-ls.nvim",
+		requires = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("SuperYuro.config.null-ls")
+		end,
+	})
 
-                rgb_fn = false,
-                hsl_fn = false,
-                css = false,
-                css_fn = false,
+	-- Completion
+	use({
+		"hrsh7th/nvim-cmp",
+		requires = {
+			"onsails/lspkind.nvim", -- Icons
+			"hrsh7th/cmp-nvim-lsp", -- from LSP
+			"hrsh7th/cmp-buffer", -- from Buffer
+			"hrsh7th/cmp-path", -- from file path
+			"hrsh7th/cmp-vsnip", -- from snippet
+			"hrsh7th/vim-vsnip", -- from snippet
+			"hrsh7th/cmp-nvim-lsp-document-symbol", -- from document symbol
+			"hrsh7th/cmp-nvim-lsp-signature-help", -- from signatures
+		},
+		config = function()
+			require("SuperYuro.config.completions")
+		end,
+	})
 
-                tailwind = true,
+	-- Git
 
-                mode = "background",
-            })
-        end,
-    })
+	-- Color preview
+	use({
+		"NvChad/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup({
+				RGB = true,
+				RRGGBB = true,
+				RRGGBBAA = false,
+				AARRGGBB = false,
+				names = false,
 
-    -- Create missing directories when saving files
-    use({
-        "jghauser/mkdir.nvim",
-    })
+				rgb_fn = false,
+				hsl_fn = false,
+				css = false,
+				css_fn = false,
 
-    -- Surround
-    use({
-        "kylechui/nvim-surround",
-        tag = "*",
-        config = function()
-            require("nvim-surround").setup({})
-        end,
-    })
+				tailwind = true,
 
-    -- Pair brackets automatically
-    use({
-        "windwp/nvim-autopairs",
-        event = "InsertEnter",
-        config = function()
-            require("nvim-autopairs").setup({
-                disable_filetype = { "TelescopePrompt", "vim" },
-            })
-        end,
-    })
+				mode = "background",
+			})
+		end,
+	})
 
-    -- Comment
-    use({
-        "numToStr/Comment.nvim",
-        config = function()
-            require("Comment").setup()
-        end,
-    })
+	-- Highlight same words
+	use({
+		"RRethy/vim-illuminate",
+		config = function()
+			require("SuperYuro.config.illuminate")
+		end,
+	})
 
-    -- Interactive interface for json
-    use({
-        "gennaro-tedesco/nvim-jqx",
-        ft = { "json", "yaml" },
-    })
+	-- Show indent line
+	use({
+		"lukas-reineke/indent-blankline.nvim",
+		config = function()
+			require("indent_blankline").setup({
+				show_current_context = true,
+				show_current_context_start = true,
+			})
+		end,
+	})
 
-    -- Auto pandoc
-    use({
-        "jghauser/auto-pandoc.nvim",
-        requires = {
-            "nvim-lua/plenary.nvim",
-        },
-        config = function()
-            require("auto-pandoc")
-        end,
-    })
+	-- Create missing directories when saving files
+	use({
+		"jghauser/mkdir.nvim",
+	})
 
-    -- Automatically set up your configuration after cloning packer.nvim
-    -- Put this at the end after all plugins
-    if packer_bootstrap then
-        require("packer").sync()
-    end
+	-- Surround
+	use({
+		"kylechui/nvim-surround",
+		tag = "*",
+		config = function()
+			require("nvim-surround").setup({})
+		end,
+	})
+
+	-- Pair brackets automatically
+	use({
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = function()
+			require("nvim-autopairs").setup({
+				disable_filetype = { "TelescopePrompt", "vim" },
+			})
+		end,
+	})
+
+	-- Comment
+	use({
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup()
+		end,
+	})
+
+	-- Interactive interface for json
+	use({
+		"gennaro-tedesco/nvim-jqx",
+		ft = { "json", "yaml" },
+	})
+
+	-- Auto pandoc
+	use({
+		"jghauser/auto-pandoc.nvim",
+		requires = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			require("auto-pandoc")
+		end,
+	})
+
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
