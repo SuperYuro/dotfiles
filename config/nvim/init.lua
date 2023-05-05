fa676738f62ab1252cd99ab1d22abd67fe9af9c2
SuperYuro <a.gpgtdmgp@gmail.com>
2023-05-05 22:53:52 +0900

    nvim: unite some configs

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,19 +1,17 @@
 require("SuperYuro.base")
-require("SuperYuro.highlights")
 require("SuperYuro.maps")
 require("SuperYuro.plugins")
 
-
 local has = function(x)
-	return vim.fn.has(x) == 1
+    return vim.fn.has(x) == 1
 end
 
 local is_mac = has("SuperYuro.macunix")
 local is_win = has("SuperYuro.win32")
 
 if is_mac then
-	require("SuperYuro.macos")
+    vim.opt.clipboard:append({ "unnamedplus" })
 end
 if is_win then
-	require("SuperYuro.windows")
+    vim.opt.clipboard:prepend({ "unnamed", "unnamedplus" })
 end
