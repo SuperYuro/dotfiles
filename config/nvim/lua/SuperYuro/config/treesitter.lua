80bf9766abcdbe940b6c17ea2f16b712d91c5200
SuperYuro <a.gpgtdmgp@gmail.com>
2023-05-08 00:35:18 +0900

    nvim: enable rainbow brackets

--- config/nvim/lua/SuperYuro/config/treesitter.lua
+++ config/nvim/lua/SuperYuro/config/treesitter.lua
@@ -31,7 +31,11 @@ require("nvim-treesitter.configs").setup({
     autotag = {
         enable = true,
     },
-    -- rainbow = { enable = true, extended_mode = true, max_file_lines = nil, },
+    rainbow = {
+        enable = true,
+        extended_mode = true,
+        max_file_lines = nil,
+    },
     endwise = {
         enable = true,
     },
