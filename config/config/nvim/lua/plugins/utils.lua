1de464de3366e996bee73c9e151d18e69a0a9669
SuperYuro <a.gpgtdmgp@gmail.com>
2024-07-29 03:06:40 +0900

    ✨ (nvim): add session manager

--- config/config/nvim/lua/plugins/utils.lua
+++ config/config/nvim/lua/plugins/utils.lua
@@ -12,4 +12,9 @@ return {
     event = "VeryLazy",
     config = true,
   },
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre", -- this will only start session saving when an actual file was opened
+    opts = {},
+  },
 }
