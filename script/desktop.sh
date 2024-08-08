e3014226e6820d0243a42e7b51cd6b9762d14ea0
SuperYuro <a.gpgtdmgp@gmail.com>
2024-08-08 23:45:16 +0900

    ✨ (install) add install script for desktop

---
+++ script/desktop.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -e
+
+# Install git
+sudo pacman -S --needed git unzip --noconfirm
+
+# Install Rust
+sudo pacman -S rustup --noconfirm
+rustup default stable
+
+# Install paru
+git clnoe https://aur.archlinux.org/paru ~/paru --depth 1
+cd ~/paru
+makepkg -si --noconfirm
+
+cd
+
+# Install Hyprland and its utils
+paru -S plasma kde-applications wl-clipboard --noconfirm
+
+# Install terminal
+paru -S kitty --noconfirm
+
+# Install themes and wallpapers
+paru -S nwg-look arc-gtk-theme papirus-icon-theme archlinux-wallpaper --noconfirm
+
+# Install fonts
+paru -S noto-fonts noto-fonts-cjk noto-fonts-extra noto-fonts-emoji ttf-mplus-nerd ttf-udev-gothic --noconfirm
+
+# Install web browser, email client and chat tool
+paru -S firefox discord --noconfirm
+
+# Install shell and its utils
+paru -S fish starship eza bat dust fd ripgrep tmux --noconfirm
+
+# Install editor
+paru -S neovim --noconfirm
+
+# Install utils for development
+paru -S clang llvm go npm yarn deno python python-pip python-setuptools mise usage --noconfirm
+
+# Install ime
+paru -S fcitx5-im fcitx5-mozc --noconfirm
+
+# Install and setup android sdk
+paru -S jdk17-openjdk android-sdk-cmdline-tools-latest android-sdk-build-tools android-sdk-platform-tools android-platform android-tools android-emulator android-google-apis-x86-64-system-image --noconfirm
+sudo groupadd android-sdk
+sudo gpasswd -a $USER android-sdk
+sudo setfacl -R -m g:android-sdk:rwx /opt/android-sdk
+sudo setfacl -d -m g:android-sdk:rwX /opt/android-sdk
+paru -S google-chrome flutter --noconfirm
+
