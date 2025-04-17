9c2ddc37af82a01aee9730bde02aa3f57da9050a
SuperYuro <a.gpgtdmgp@gmail.com>
2025-04-18 03:46:23 +0900

    chore(nvim): remove some plugins

--- config/config/nvim/lua/plugins/lsp.lua
+++ config/config/nvim/lua/plugins/lsp.lua
@@ -25,8 +25,6 @@ local ensure_installed = {
     "ruff",
 
     "taplo",
-
-    "omnisharp",
   },
   tools = {
     -- Formatters
@@ -51,8 +49,6 @@ local ensure_installed = {
 local builtins = require("conform-selector.builtins")
 local js_formatters = builtins.javascript.formatters()
 
-local browser = require("utils.exec").browser
-
 return {
   {
     "neovim/nvim-lspconfig",
@@ -60,9 +56,6 @@ return {
       -- LSP Installer
       "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
-
-      -- Completion from LSP
-      "hrsh7th/cmp-nvim-lsp",
     },
     lazy = false,
     config = function()
@@ -104,101 +97,12 @@ return {
   },
   {
     "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+    },
     opts = {
       ensure_installed = ensure_installed.lsp,
       automatic_installation = false,
-    },
-  },
-  {
-    "nvimdev/lspsaga.nvim",
-    event = "LspAttach",
-    cmd = { "Lspsaga" },
-    keys = {
-      { "<leader>lf", "<cmd>Lspsaga finder<cr>", desc = "Lspsaga finder" },
-      { "<leader>rn", "<cmd>Lspsaga rename<cr>", desc = "Lspsaga rename" },
-      { "K", "<cmd>Lspsaga hover_doc<cr>", desc = "Hover doc" },
-      { "<leader>ca", "<cmd>Lspsaga code_action<cr>", desc = "Code action" },
-      { "gd", "<cmd>Lspsaga goto_definition<cr>", desc = "Goto definition" },
-      { "gD", "<cmd>Lspsaga goto_type_definition<cr>", desc = "Goto type definition" },
-      { "g]", "<cmd>Lspsaga diagnostic_jump_next<cr>", desc = "Goto next diagnostics" },
-      { "g[", "<cmd>Lspsaga diagnostic_jump_prev<cr>", desc = "Goto previous diagnostics" },
-      { "<leader>e", "<cmd>Lspsaga show_line_diagnostics<cr>", desc = "Show line diagnostics" },
-    },
-    opts = {
-      lightbulb = {
-        enable = false,
-      },
-      finder = {
-        layout = "float",
-        silent = false,
-        keys = {
-          toggle_or_open = { "o", "<space>" },
-          vsplit = { "<C-v>" },
-          tabnew = { "<C-t>" },
-          quit = { "q", "<ESC>" },
-        },
-      },
-      rename = {
-        in_select = true,
-        auto_save = true,
-        keys = {
-          exec = { "<CR>" },
-          quit = { "<ESC>" },
-        },
-      },
-      hover_doc = {
-        open_cmd = browser(),
-      },
-      code_action = {
-        num_shortcut = true,
-        show_server_name = true,
-        extend_gitsigns = false,
-        keys = {
-          quit = { "q", "<ESC>" },
-          exec = { "<CR>" },
-        },
-      },
-      symbol_in_winbar = {
-        enable = false,
-        separator = "  ",
-        show_file = true,
-      },
-      diagnostics = {
-        show_code_action = true,
-        keys = {
-          quit = { "q", "<ESC>" },
-        },
-      },
-    },
-  },
-  {
-    "j-hui/fidget.nvim",
-    event = "LspAttach",
-    opts = {
-      notification = {
-        window = {
-          winblend = 0,
-        },
-      },
-    },
-  },
-  {
-    "folke/trouble.nvim",
-    event = "LspAttach",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    keys = {
-      { "<leader>tr", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Show workspace diagnostics" },
-    },
-    opts = {
-      position = "bottom",
-      height = 20,
-      action_keys = {
-        close = { "q", "<esc>" },
-        cancel = {},
-      },
-      use_diagnostics_signs = true,
     },
   },
   {
@@ -206,7 +110,7 @@ return {
     lazy = false,
     opts = {
       ensure_installed = ensure_installed.tools,
-      auto_update = false,
+      auto_update = true,
       run_on_start = true,
     },
   },
@@ -261,23 +165,12 @@ return {
   },
   {
     "folke/lazydev.nvim",
-    ft = { "lua" },
-    dependencies = {
-      { "Bilal2453/luvit-meta", lazy = true },
-      {
-        "hrsh7th/nvim-cmp",
-        opts = function(_, opts)
-          opts.sources = opts.sources or {}
-          table.insert(opts.sources, {
-            name = "lazydev",
-            group_index = 0,
-          })
-        end,
-      },
-    },
+    ft = "lua", -- only load on lua files
     opts = {
       library = {
-        "lazy.nvim",
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
       },
     },
   },
