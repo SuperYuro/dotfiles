5fa4f4736e7c4fade353d7bfdf7e721a1a0310f7
SuperYuro <a.gpgtdmgp@gmail.com>
2025-08-26 21:32:18 +0900

    feat(nvim): configure lsp for vue

--- config/config/nvim/lua/plugins/lsp.lua
+++ config/config/nvim/lua/plugins/lsp.lua
@@ -64,8 +64,39 @@ return {
     event = "LspAttach",
     opts = {
       ensure_installed = ensure_installed.lsp,
-      automatic_enable = true,
+      automatic_enable = {
+        exclude = {
+          "vue_ls",
+        },
+      },
     },
+    config = function(_, opts)
+      require("mason-lspconfig").setup(opts)
+
+      local vue_language_server_path = vim.fn.expand("$MASON/packages") .. "/vue-language-server" .. "/node_modules/@vue/language-server"
+      local tsserver_filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
+      local vue_plugin = {
+        name = "@vue/typescript-plugin",
+        location = vue_language_server_path,
+        languages = { "vue" },
+        configNamespace = "typescript",
+      }
+
+      local ts_ls_config = {
+        init_options = {
+          plugins = {
+            vue_plugin,
+          },
+        },
+        filetypes = tsserver_filetypes,
+      }
+
+      local vue_ls_config = {}
+      vim.lsp.config("vue_ls", vue_ls_config)
+      vim.lsp.config("ts_ls", ts_ls_config)
+
+      vim.lsp.enable({ "tsls", "vue_ls" }) -- If using `ts_ls` replace `vtsls` to `ts_ls`
+    end,
   },
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
